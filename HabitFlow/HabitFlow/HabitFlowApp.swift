2f84c23d26e681a333642638d0a6220658bf0d1d
kenshin0413 <kenshin1234569@gmail.com>
2025-11-12 00:44:44 +0900

    初期

---
+++ HabitFlow/HabitFlow/HabitFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitFlowApp.swift
+//  HabitFlow
+//
+//  Created by miyamotokenshin on R 7/11/12.
+//
+
+import SwiftUI
+
+@main
+struct HabitFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
